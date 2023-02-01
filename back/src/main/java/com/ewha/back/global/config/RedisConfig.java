85767e25a99ae303090e294eb53a3f4c77025e58
gnidinger <gcnb87@gmail.com>
2023-02-01 16:46:35 +0900

    Jenkins-Test

--- back/src/main/java/com/ewha/back/global/config/RedisConfig.java
+++ back/src/main/java/com/ewha/back/global/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.ewha.back.global.config;
 
 import com.ewha.back.domain.chat.service.RedisSubscriber;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -25,78 +26,78 @@ import static com.ewha.back.global.config.CacheConstant.*;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
+	@Value("${spring.redis.host}")
+	private String redisHost;
 
-    @Value("${spring.redis.port}")
-    private int redisPort;
+	@Value("${spring.redis.port}")
+	private int redisPort;
 
-    /*
-     * 단일 Topic 사용을 위해 Channel Topic 빈으로 등록
-     */
-    @Bean
-    public ChannelTopic channelTopic() {
-        return new ChannelTopic("chatroom");
-    }
+	/*
+	 * 단일 Topic 사용을 위해 Channel Topic 빈으로 등록
+	 */
+	@Bean
+	public ChannelTopic channelTopic() {
+		return new ChannelTopic("chatroom");
+	}
 
-    /*
-     * Redis Channel(Topic)으로부터 메시지를 받고 주입된 Listener에게 비동기적으로 발송하는 역할 수행
-     */
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
-                                                                       MessageListenerAdapter listenerAdapter,
-                                                                       ChannelTopic channelTopic) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.addMessageListener(listenerAdapter, channelTopic);
-        container.setConnectionFactory(connectionFactory);
-        return container;
-    }
+	/*
+	 * Redis Channel(Topic)으로부터 메시지를 받고 주입된 Listener에게 비동기적으로 발송하는 역할 수행
+	 */
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
+		MessageListenerAdapter listenerAdapter,
+		ChannelTopic channelTopic) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.addMessageListener(listenerAdapter, channelTopic);
+		container.setConnectionFactory(connectionFactory);
+		return container;
+	}
 
-    /*
-     * Container로부터 메시지를 받아 실제 처리하는 로직
-     */
-    @Bean
-    public MessageListenerAdapter messageListenerAdapter(RedisSubscriber redisSubscriber) {
-        return new MessageListenerAdapter(redisSubscriber, "sendMessage");
-    }
+	/*
+	 * Container로부터 메시지를 받아 실제 처리하는 로직
+	 */
+	@Bean
+	public MessageListenerAdapter messageListenerAdapter(RedisSubscriber redisSubscriber) {
+		return new MessageListenerAdapter(redisSubscriber, "sendMessage");
+	}
 
-    /*
-     * Redis Server와 상호작용 하기 위한 설정. 직렬화 설정 추가
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
-        return redisTemplate;
-    }
+	/*
+	 * Redis Server와 상호작용 하기 위한 설정. 직렬화 설정 추가
+	 */
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(connectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+		return redisTemplate;
+	}
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
-    }
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
 
-    @Bean
-    public RedisCacheManager redisCacheManager() {
-        RedisCacheConfiguration redisCacheConfig = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .disableCachingNullValues()
-                .serializeKeysWith(RedisSerializationContext
-                        .SerializationPair
-                        .fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext
-                        .SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+	@Bean
+	public RedisCacheManager redisCacheManager() {
+		RedisCacheConfiguration redisCacheConfig = RedisCacheConfiguration
+			.defaultCacheConfig()
+			.disableCachingNullValues()
+			.serializeKeysWith(RedisSerializationContext
+				.SerializationPair
+				.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext
+				.SerializationPair
+				.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
-        Map<String, RedisCacheConfiguration> cacheConfiguration = new HashMap<>();
+		Map<String, RedisCacheConfiguration> cacheConfiguration = new HashMap<>();
 
-        cacheConfiguration.put(NEWEST_FEEDS, redisCacheConfig.entryTtl(Duration.ofMinutes(3)));
-        cacheConfiguration.put(FEED_COMMENTS, redisCacheConfig.entryTtl(Duration.ofMinutes(3)));
+		cacheConfiguration.put(NEWEST_FEEDS, redisCacheConfig.entryTtl(Duration.ofMinutes(3)));
+		cacheConfiguration.put(FEED_COMMENTS, redisCacheConfig.entryTtl(Duration.ofMinutes(3)));
 
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory())
-                .cacheDefaults(redisCacheConfig)
-                .build();
-    }
+		return RedisCacheManager.RedisCacheManagerBuilder
+			.fromConnectionFactory(redisConnectionFactory())
+			.cacheDefaults(redisCacheConfig)
+			.build();
+	}
 }
