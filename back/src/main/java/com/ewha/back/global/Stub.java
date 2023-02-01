db599fdbe7a17abb4428ed2c1b4b9d930511f72a
gnidinger <gcnb87@gmail.com>
2023-02-01 14:59:10 +0900

    Jenkins-Test

--- back/src/main/java/com/ewha/back/global/Stub.java
+++ back/src/main/java/com/ewha/back/global/Stub.java
@@ -1,5 +1,14 @@
 package com.ewha.back.global;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ewha.back.domain.category.entity.Category;
 import com.ewha.back.domain.category.entity.CategoryType;
 import com.ewha.back.domain.category.repository.CategoryRepository;
@@ -16,112 +25,103 @@ import com.ewha.back.domain.user.entity.enums.AgeType;
 import com.ewha.back.domain.user.entity.enums.GenderType;
 import com.ewha.back.domain.user.repository.UserRepository;
 import com.ewha.back.domain.user.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 // @Configuration
 public class Stub {
 
-    private static final Logger log = LoggerFactory.getLogger(Stub.class);
+	private static final Logger log = LoggerFactory.getLogger(Stub.class);
 
-    @Bean
-    @Transactional
-    CommandLineRunner stubInit(UserRepository userRepository, UserService userService,
-                               CategoryRepository categoryRepository, FeedRepository feedRepository,
-                               FeedService feedService, CategoryService categoryService,
-                               CommentRepository commentRepository, BCryptPasswordEncoder encoder) {
+	@Bean
+	@Transactional
+	CommandLineRunner stubInit(UserRepository userRepository, UserService userService,
+		CategoryRepository categoryRepository, FeedRepository feedRepository,
+		FeedService feedService, CategoryService categoryService,
+		CommentRepository commentRepository, BCryptPasswordEncoder encoder) {
 
-        for (int i = 0; i < 12; i++) {
-            Category category = Category.builder()
-                    .categoryType(CategoryType.values()[i])
-                    .build();
-            categoryRepository.save(category);
-        }
+		for (int i = 0; i < 12; i++) {
+			Category category = Category.builder()
+				.categoryType(CategoryType.values()[i])
+				.build();
+			categoryRepository.save(category);
+		}
 
-//         ------------------------------------------------------------------------------------------
-//         USER STUB
-//         ------------------------------------------------------------------------------------------
-        for (int i = 1; i <= 20; i++) {
+		//         ------------------------------------------------------------------------------------------
+		//         USER STUB
+		//         ------------------------------------------------------------------------------------------
+		for (int i = 1; i <= 20; i++) {
 
-            Long rand = (long) ((Math.random() * 12) + 1);
+			Long rand = (long)((Math.random() * 12) + 1);
 
-            UserCategory userCategory = UserCategory.builder().build();
+			UserCategory userCategory = UserCategory.builder().build();
 
-            Category category = new Category();
-            category.setId(rand);
+			Category category = new Category();
+			category.setId(rand);
 
-            userCategory.addCategory(category);
+			userCategory.addCategory(category);
 
-            User user = User.builder()
-                    .userId("asdfasdfasdf" + i)
-                    .ariFactor(36.5)
-                    .password(encoder.encode("1234"))
-                    .nickname("qwer" + i)
-                    .userCategories(List.of(userCategory))
-                    .role(List.of("USER"))
-                    .isFirstLogin(false)
-                    .genderType(GenderType.MALE)
-                    .ageType(AgeType.THIRTIES)
-                    .profileImage("profile image")
-                    .build();
+			User user = User.builder()
+				.userId("asdfasdfasdf" + i)
+				.ariFactor(36.5)
+				.password(encoder.encode("1234"))
+				.nickname("qwer" + i)
+				.userCategories(List.of(userCategory))
+				.role(List.of("USER"))
+				.isFirstLogin(false)
+				.genderType(GenderType.MALE)
+				.ageType(AgeType.THIRTIES)
+				.profileImage("profile image")
+				.build();
 
-            userCategory.addUser(user);
+			userCategory.addUser(user);
 
-            log.info("USER STUB " + userRepository.save(user));
-        }
-//         ------------------------------------------------------------------------------------------
-//         ------------------------------------------------------------------------------------------
-//         FEED STUB
-//         ------------------------------------------------------------------------------------------
-        for (int i = 1; i <= 40; i++) {
+			log.info("USER STUB " + userRepository.save(user));
+		}
+		//         ------------------------------------------------------------------------------------------
+		//         ------------------------------------------------------------------------------------------
+		//         FEED STUB
+		//         ------------------------------------------------------------------------------------------
+		for (int i = 1; i <= 40; i++) {
 
-            Long rand = (long) ((Math.random() * 11) + 1);
+			Long rand = (long)((Math.random() * 11) + 1);
 
-            Category category = Category.builder()
-                    .id(rand)
-                    .categoryType(CategoryType.CULTURE)
-                    .build();
+			Category category = Category.builder()
+				.id(rand)
+				.categoryType(CategoryType.CULTURE)
+				.build();
 
-            FeedCategory feedCategory = FeedCategory.builder()
-                    .category(category)
-                    .build();
+			FeedCategory feedCategory = FeedCategory.builder()
+				.category(category)
+				.build();
 
-            Feed feed = Feed.builder()
-                    .feedCategories(List.of(feedCategory))
-                    .user(userService.findVerifiedUser((long) ((Math.random() * 20) + 1)))
-                    .title("title" + i)
-                    .body("body" + i)
-                    .likeCount((long) (Math.random() * 50))
-                    .viewCount((long) (Math.random() * 100))
-                    .build();
+			Feed feed = Feed.builder()
+				.feedCategories(List.of(feedCategory))
+				.user(userService.findVerifiedUser((long)((Math.random() * 20) + 1)))
+				.title("title" + i)
+				.body("body" + i)
+				.likeCount((long)(Math.random() * 50))
+				.viewCount((long)(Math.random() * 100))
+				.build();
 
-            feedCategory.addFeed(feed);
+			feedCategory.addFeed(feed);
 
-            log.info("FEED STUB " + feedRepository.save(feed));
-        }
-//         ------------------------------------------------------------------------------------------
-//         ------------------------------------------------------------------------------------------
-//         COMMENT STUB
-//         ------------------------------------------------------------------------------------------
-        for (int i = 1; i <= 80; i++) {
+			log.info("FEED STUB " + feedRepository.save(feed));
+		}
+		//         ------------------------------------------------------------------------------------------
+		//         ------------------------------------------------------------------------------------------
+		//         COMMENT STUB
+		//         ------------------------------------------------------------------------------------------
+		for (int i = 1; i <= 80; i++) {
 
-            Comment comment = Comment.builder()
-                    .body("comment body" + i)
-                    .user(userService.findVerifiedUser((long) (Math.random() * 20) + 1))
-                    .feed(feedService.findVerifiedFeed((long) (Math.random() * 40) + 1))
-                    .likeCount((long) (Math.random() * 10) + 1)
-                    .build();
+			Comment comment = Comment.builder()
+				.body("comment body" + i)
+				.user(userService.findVerifiedUser((long)(Math.random() * 20) + 1))
+				.feed(feedService.findVerifiedFeed((long)(Math.random() * 40) + 1))
+				.likeCount((long)(Math.random() * 10) + 1)
+				.build();
 
-            log.info("COMMENT STUB " + commentRepository.save(comment));
-        }
-//         ------------------------------------------------------------------------------------------
-        return null;
-    }
+			log.info("COMMENT STUB " + commentRepository.save(comment));
+		}
+		//         ------------------------------------------------------------------------------------------
+		return null;
+	}
 }
