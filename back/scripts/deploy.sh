fdd9ca084f8e8ed1ef6b29f0005a04329607ef53
gnidinger <gcnb87@gmail.com>
2023-02-01 12:38:03 +0900

    Jenkins-Test

--- back/scripts/deploy.sh
+++ back/scripts/deploy.sh
@@ -1,11 +1,11 @@
 # 가동중인 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=ewhaproject" | grep -q . && docker stop ewhaproject && docker rm ewhaproject | true
+sudo docker ps -a -q --filter "name=ewha_project" | grep -q . && docker stop ewhaproject && docker rm ewhaproject | true
 
 # 기존 이미지 삭제
-sudo docker rmi gnidigner/ewhaproject:1.0
+sudo docker rmi gnidigner/ewha_project:1.0
 
 # 도커허브 이미지 pull
-sudo docker pull gnidigner/ewhaproject:1.0
+sudo docker pull gnidigner/ewha_project:1.0
 
 # 도커 run
 docker run -d -p 8080:8080 --network my-network -v /home/ubuntu:/config --name ewhaproject gnidigner/ewhaproject:1.0
